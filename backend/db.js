fb9aa659a7c82465bed27bc400e5d552325c53b0
Amit <itsamit108@outlook.com>
2023-06-12 00:25:05 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = 'mongodb+srv://itsamit108:itsamit108@cluster0.yknaszp.mongodb.net/?retryWrites=true&w=majority';
+const mongoURI = 'mongodb+srv://itsamit108:YoQwgheThIls9jaG@cluster0.yknaszp.mongodb.net/?retryWrites=true&w=majority';
 
 const connectToMongo = async () => {
     try {
