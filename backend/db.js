3082a16c3d62521705a6f669b5a7a4ad5ca5d272
Amit <itsamit108@outlook.com>
2023-12-17 15:59:43 +0000

    Update MongoDB connection string and fix variable declaration

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = 'mongodb+srv://itsamit108:YoQwgheThIls9jaG@cluster0.yknaszp.mongodb.net/?retryWrites=true&w=majority';
+const mongoURI = 'mongodb+srv://itsamit108:NqMVKbmo3X1EY8Fq@notesync.dryaw4g.mongodb.net/?retryWrites=true&w=majority';
 
 const connectToMongo = async () => {
     try {
